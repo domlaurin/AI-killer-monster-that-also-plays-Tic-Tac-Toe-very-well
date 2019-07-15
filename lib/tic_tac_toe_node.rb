735e65d6f7664bfeae27a62ebd1bd4d5038a4633
Dom <dominykas.laurinavicius@gmail.com>
2019-07-15 13:03:13 +0300

    Change #losing_node? base case

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -3,16 +3,15 @@ require_relative 'tic_tac_toe'
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
-    @next_mover_mark = next_mover_mark
+    @next_mover_mark = next_mover_mark #player who will move next
     @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
-    winning_player = self.players[self.board.winner]
-    return true if @board.over? && winning_player != #us
-    return false if @board.over? && winning_player == #us || nil
+    return true if @board.over? && @board.winner != @next_mover_mark
+    return false if @board.over? && @board.winner == @next_mover_mark || nil
 
-    
+
   end
 
   def winning_node?(evaluator)
